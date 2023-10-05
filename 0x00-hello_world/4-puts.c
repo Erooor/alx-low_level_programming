72407ef5faf95c6f35e662749c32975351abb176
Erooor <elmouaffaks@gmail.com>
2023-10-05 12:30:53 -0700

    OK

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * Description: Print "Programming is like building a multilingual puzzle"
- * 'main()' - function prints the string
+ * Print an output
  *
- * "Programming is like building a multilingual puzzle"
- * 'return (0)' - on success
+ * @main The function of printing
+ * @return The indication that it terminated successfully 
  */
 int main(void)
 {

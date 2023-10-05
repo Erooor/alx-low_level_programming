6ee054072146f0455d51a77858624726774e3224
Erooor <elmouaffaks@gmail.com>
2023-10-05 11:49:00 -0700

    OK

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * The main() function prints the string
+ * The main()- function prints the string
  * "Programming is like building a multilingual puzzle" to the standard output.
  *
  * Inputs: None
  * Outputs: The string "Programming is like building a multilingual puzzle"
- * Return value: 0 on success
+ * Return - value: 0 on success
  */
 int main(void)
 {

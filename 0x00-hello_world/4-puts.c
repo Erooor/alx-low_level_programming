c3503d4669f10c6e66fa643e96fd2a255f4d0f16
Erooor <elmouaffaks@gmail.com>
2023-10-05 12:07:05 -0700

    OK

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * Print "Programming is like building a multilingual puzzle"
+ * Description: Print "Programming is like building a multilingual puzzle"
  * 'main()' - function prints the string
  *
- * Description: function prints The string
  * "Programming is like building a multilingual puzzle"
  * 'return (0)' - on success
  */

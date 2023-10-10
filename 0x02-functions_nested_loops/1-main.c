ccc32570ed8de83a7eae77a2ae0fc90dc5b22c18
Erooor <elmouaffaks@gmail.com>
2023-10-10 16:11:38 -0700

    OK

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code for Holberton School students.
@@ -6,15 +7,6 @@
  * Return: Always 0.
  */
 void print_alphabet(void)
-{
-int c = 'a';
-while (c <= 'z')
-{
-putchar(c);
-c++;
-}
-putchar('\n');
-}
 int main(void)
 {
 	print_alphabet();

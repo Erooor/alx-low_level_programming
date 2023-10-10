cc7e49ee310f287d922bb1fdbd4474e5be3ea520
Erooor <elmouaffaks@gmail.com>
2023-10-10 13:06:43 -0700

    OK

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * 
+ * Description: Print alphabet
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    
+int n = 'a';
+
+while (n <= 'z')
+{
+putchar(n);
+putchar('\n');
+n++;
+}
+return (0);
+}

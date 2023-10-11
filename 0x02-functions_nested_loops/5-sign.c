a5026a35c42a67eb88c6f3bd9a4426921e5f78f6
Erooor <elmouaffaks@gmail.com>
2023-10-11 09:28:49 -0700

    OK

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_sign - Check numbers if possitive or negative or 0
+ * @n: parametr to check
+ *
+ * Description: Return 1 if n possitive, Return 0 if n is 0
+ * else return -1
+ *
+ * Return: Always 0 (success)
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+if (n == 0)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,5 +6,7 @@ int _islower(int c);
 int _putchar(int c);
 int _isalpha(int c);
 int _putchar(int c);
+int print_sign(int n);
+
 
 

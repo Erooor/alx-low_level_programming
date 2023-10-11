ff0c1fb2d7b18d14b19d2f13341689eaef358fe3
Erooor <elmouaffaks@gmail.com>
2023-10-11 16:22:50 -0700

    OK

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * add - calculat the the sum of two integer
+ *
+ * Description: This function calculate the sum of two int and
+ * return the sum
+ *
+ * Return: Always 0 (success)
+ */
+
+int add(int , int )
+{
+int sum = add(int, int);
+return (sum);
+}

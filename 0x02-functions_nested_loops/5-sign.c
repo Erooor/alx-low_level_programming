06dd059f6c79892b01c518df923d2418f5d7bcca
Erooor <elmouaffaks@gmail.com>
2023-10-11 10:31:04 -0700

    OK

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,17 +13,15 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-print_sign(n);
-return (1);
+putchar('+'); return (1);
 }
 if (n == 0)
 {
-print_sign(n);
-return (0);
+
+putchar('0'); return (0);
 }
 else
 {
-print_sign(n);
-return (-1);
+putchar('-'); return (-1);
 }
 }

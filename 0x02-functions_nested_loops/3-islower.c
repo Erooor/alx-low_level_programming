39d8efe88cbc02c2531f6ace84bbcb47f786bffd
Erooor <elmouaffaks@gmail.com>
2023-10-11 07:47:34 -0700

    OK

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,16 +5,18 @@
  *
  * Description: Return 1 in lowercase else Return 0
  *
+ * @param c - parametr to check
+ *
  * Return: Always 0 (success)
  */
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
-return 1;
+return (1);
 }
 else
 {
-return 0;
+return (0);
 }
 }

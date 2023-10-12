54c26ac2fcf0a433268a0f19258c40adb74b2ef6
Erooor <elmouaffaks@gmail.com>
2023-10-12 08:56:35 -0700

    OK

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - Return a value if c a digit
+ * @a: Number one
+ * @b: Number two
+ * Return: 0 (Sucess)
+ */
+int mul(int a, int b)
+{
+int mul = a * b;
+return(mul);
+}
+

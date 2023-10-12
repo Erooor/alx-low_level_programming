f10edb7396a501c166f01425aab755abe0088dd6
Erooor <elmouaffaks@gmail.com>
2023-10-12 14:10:19 -0700

    OK

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_most_numbers - Print number from 0 to 9 noexcept
+ * 2 and 4
+ * 
+ * Return: 0 (Sucess)
+ */
+void print_most_numbers(void)
+{
+    int c = 0;
+    while (c <= 9)
+    {if (c != 2 && c != 4)
+    {
+    putchar(c + '0');
+    }
+    c++;
+    }
+    putchar('\n');
+}

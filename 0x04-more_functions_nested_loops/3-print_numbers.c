23013977ba75aee8a245953b1a9485f042053695
Erooor <elmouaffaks@gmail.com>
2023-10-12 09:15:01 -0700

    OK

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - Print number from 0 to 9
+ * 
+ * Return: 0 (Sucess)
+ */
+void print_numbers(void)
+{
+    int a = 0;
+    while (a <= 9)
+    {putchar(a + '0');
+    a++;
+    }
+    putchar('\n');
+}

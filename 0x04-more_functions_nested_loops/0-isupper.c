406629ff83393931f9819fc1eeb6e1c47858f31f
Erooor <elmouaffaks@gmail.com>
2023-10-12 08:08:45 -0700

    OK

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _isupper - Return a value if c an upper or 
+ * lowercase
+ * @c: letter to check
+ *
+ * Return: 1 if upper or 0 if lower
+ */
+int _isupper(int c);
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return(1);
+    }
+    else
+    {
+        return(0);
+    }
+}

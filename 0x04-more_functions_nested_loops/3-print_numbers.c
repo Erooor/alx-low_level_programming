40f3db3b49003cd4704cf5d0a9f448160702181c
Erooor <elmouaffaks@gmail.com>
2023-10-13 06:57:39 -0700

    OK

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,16 @@
 
 /**
  * print_numbers - Print number from 0 to 9
- * 
+ *
  * Return: 0 (Sucess)
  */
 void print_numbers(void)
 {
-    int a = 0;
-    while (a <= 9)
-    {putchar(a + '0');
-    a++;
-    }
-    putchar('\n');
+int a = 0;
+while (a <= 9)
+{
+putchar(a + '0');
+a++;
+}
+putchar('\n');
 }

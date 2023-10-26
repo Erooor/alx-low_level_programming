e88c271b8bfda034a0993056a06109ba402aa533
Erooor <elmouaffaks@gmail.com>
2023-10-26 16:28:49 -0700

    OK

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - main function
+  *
+  * @s: function parameter
+  *
+  * Return: Always 0.
+  */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+putchar(*s);
+puts_recursion(s + 1);
+}
+else
+putchar('\n');
+}

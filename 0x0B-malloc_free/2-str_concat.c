1adc02f9196e6c901ebc8a8504f61ffa7b914b94
Erooor <elmouaffaks@gmail.com>
2023-10-31 15:34:50 -0700

    OK

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,12 +15,14 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 		i = j = 0;
 	while (s1[i] != '\0')
 		i++;
-	while (s2[ci] != '\0')
-		ci++;
+	while (s2[j] != '\0')
+		j++;
 	new = malloc(sizeof(char) * (i + j + 1));
 	if (new == NULL)
 		return (NULL);

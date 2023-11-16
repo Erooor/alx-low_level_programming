7409cd3c049184f84a530488dc2bab92e9260571
Erooor <elmouaffaks@gmail.com>
2023-11-16 04:29:48 -0800

    OK

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,27 +3,28 @@
 
 /**
  * print_list - prints all the elements of a list_t
- * @*h: Struct content
- * 
+ * @h: Struct content
+ *
  * Return: the numbers of nodes
  */
+
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-        {
-            printf("[0] (nil)");
-        }
-        else
-        {
-            printf("%s", h->str);
-        }
-        count++;
-        h = h->next;
-    }
-    printf("\n");
-    return (count);
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+count++;
+h = h->next;
+}
+printf("\n");
+return (count);
 }
